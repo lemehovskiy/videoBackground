9aecb1e780beab7954716a663ae07d19124050ca
lemehovskiy <lemehovskiy@gmail.com>
2018-01-13 22:51:58 +0200

    fix require

--- build/videoBackground.js
+++ build/videoBackground.js
@@ -9,17 +9,7 @@
 
  */
 
-;(function (factory) {
-    'use strict';
-
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
-    }
-})(function ($) {
+(function ($) {
 
     $.fn.videoBackground = function (options) {
 
@@ -84,4 +74,4 @@
             }
         }
     };
-});
+})(jQuery);
--- src/videoBackground.es6
+++ src/videoBackground.es6
@@ -7,19 +7,8 @@
 
  */
 
-;(function (factory) {
-    'use strict';
-
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
-    }
-})
-
 (function ($) {
+
 
     $.fn.videoBackground = function (options) {
 
@@ -90,4 +79,5 @@
 
     };
 
-});
+
+})(jQuery);
